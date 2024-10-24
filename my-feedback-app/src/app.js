368c90f00ca059411a31f00c0e9889858f491bf5
ediaz13 <diazemanuel27@gmail.com>
2024-10-24 00:51:02 -0300

    Managing Global State

--- my-feedback-app/src/app.js
+++ my-feedback-app/src/app.js
@@ -1,13 +1,17 @@
+import { useState } from "react"
 import Header from "./components/Header"
 import FeedbackItem from "./components/FeedbackItem"
+import FeedBackData from "./data/FeedbackData"
+import FeedBackList from "./components/FeedBackList"
 
 function App() {
+    const [feedback, setFeedback] = useState(FeedBackData)
    
     return (
         <>
             <Header />
             <div className="container">
-                <FeedbackItem />
+                <FeedBackList feedback={ feedback } />
             </div>
         </>
     )
